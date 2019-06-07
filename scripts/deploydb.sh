d55adbb9d50b2edbf86f08a5399b529f3dc202c3
Tola Awofolu <oawofolu@pivotal.io>
2019-06-06 23:17:29 -0400

    Updated directory path

--- scripts/deploydb.sh
+++ scripts/deploydb.sh
@@ -1,21 +1,23 @@
+#!/bin/sh
+
 ##Install required utilities
-#yum -y update
-#yum install -y zip
+yum -y update
+yum install -y zip
 
 ## Create output folder to store logs
-#mkdir -p db_logs
+mkdir -p db_logs
 
 ## Loop through contents of zip
-#for zipfile in ./db_source_files/dbfiles/*.zip; do
-#   unzip $zipfile
-#   zipfilename=`basename $zipfile .zip`
-#   for inputfile in $zipfilename/*; do
-#      inputfilename=`basename $inputfile`
+for zipfile in ./db_source_files/dbfiles/*.zip; do
+   unzip $zipfile
+   zipfilename=`basename $zipfile .zip`
+   for inputfile in $zipfilename/*; do
+      inputfilename=`basename $inputfile`
 
       ## Execute SQL script and generate logs
-      #echo exit | sqlplus -s $USERNAME/$PASSWD@//$HOST/$SID @"$inputfile" > db_logs/"$inputfilename".txt | echo "Log generation complete for script: ${inputfilename}"
-   #done     
-#done
+      echo exit | sqlplus -s $USERNAME/$PASSWD@//$HOST/$SID @"$inputfile" > db_logs/"$inputfilename".txt | echo "Log generation complete for script: ${inputfilename}"
+   done     
+done
 
 ##Debugging: View contents of log directory
 ls -ltr db_logs
