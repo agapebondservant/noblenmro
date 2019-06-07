3eb1473c42e50feda98ce2638f76bac1829fc054
Tola Awofolu <oawofolu@pivotal.io>
2019-06-07 09:23:17 -0400

    Edited script

--- scripts/deploydb.sh
+++ scripts/deploydb.sh
@@ -22,6 +22,6 @@ for zipfile in ./db_source_files/dbfiles/*.zip; do
    for inputfile in $directoryname/*; do
       inputfilename=`basename $inputfile`
       ## Execute SQL script and generate logs
-      echo exit | sqlplus -s $USERNAME/$PASSWD@//$HOST/$SID @"$inputfile" > db_logs/db_deploy_file_ver"$version"_"$inputfilename".txt | echo "Log generation complete for script - log name: db_deploy_file_ver${version}_${inputfilename}.txt"
+      echo exit | sqlplus -s $USERNAME/$PASSWD@//$HOST/$SID @"$inputfile" > db_logs/db_deploy_file_ver"$version"_"$inputfilename".txt | echo "Log generation complete for script - log file name: db_deploy_file_ver${version}_${inputfilename}.txt"
    done     
 done
