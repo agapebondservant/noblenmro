6dac2f391122d7697380deef0696031ec7a6baeb
Tola Awofolu <oawofolu@pivotal.io>
2019-06-07 02:02:55 -0400

    Modified pipeline

--- scripts/deploydb.sh
+++ scripts/deploydb.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-##Install required utilities
-yum -y update
-yum install -y unzip
-
 ## Create output folder to store logs
 mkdir -p db_logs
 
@@ -11,13 +7,14 @@ mkdir -p db_logs
 for zipfile in ./db_source_files/dbfiles/*.zip; do
    unzip $zipfile
    zipfilename=`basename $zipfile .zip`
+   ## There must be a version associated with the file (TODO: pass versions from the pipeline instead)
+   if ! [[ $zipfile =~ (^[0-9\.]+)_.* ]]; then 
+	echo "ERROR: ${zipfilename} must have a version number associated. Expected format: <version no>_${zipfilename}.zip"; 
+        exit -1; 
+   else version=${BASH_REMATCH[1]}; fi
    for inputfile in $zipfilename/*; do
       inputfilename=`basename $inputfile`
-
       ## Execute SQL script and generate logs
-      echo exit | sqlplus -s $USERNAME/$PASSWD@//$HOST/$SID @"$inputfile" > db_logs/"$inputfilename".txt | echo "Log generation complete for script: ${inputfilename}"
+      echo exit | sqlplus -s $USERNAME/$PASSWD@//$HOST/$SID @"$inputfile" > db_logs/"$inputfilename"_ver"$version".txt | echo "Log generation complete for script: ${inputfilename}"
    done     
 done
-
-##Debugging: View contents of log directory
-ls -ltr db_logs
