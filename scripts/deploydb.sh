1d017a3c08807692d4e659984f22fa7a19b595bb
Tola Awofolu <oawofolu@pivotal.io>
2019-06-07 07:52:19 -0400

    Edited script

--- scripts/deploydb.sh
+++ scripts/deploydb.sh
@@ -12,13 +12,13 @@ for zipfile in ./db_source_files/dbfiles/*.zip; do
    unzip $zipfile
    zipfilename=`basename $zipfile .zip`
    ## There must be a version associated with the file (TODO: pass versions from the pipeline instead)
-   if ! [[ $zipfilename =~ (^[0-9\.]+)_.* ]]; then 
-	echo "ERROR: ${zipfilename} must have a version number associated. Expected format: <version no>_${zipfilename}.zip"; 
+   if ! [[ $zipfilename =~ (^[0-9\.]+)_DbDeployFile_.* ]]; then 
+	echo "ERROR: ${zipfilename} must have a version number associated. Expected format: <version no>_DbDeployFile_<filename>.zip"; 
         exit -1; 
    else version=${BASH_REMATCH[1]}; fi
    for inputfile in $zipfilename/*; do
       inputfilename=`basename $inputfile`
       ## Execute SQL script and generate logs
-      echo exit | sqlplus -s $USERNAME/$PASSWD@//$HOST/$SID @"$inputfile" > db_logs/"$inputfilename"_ver"$version".txt | echo "Log generation complete for script: ${inputfilename}"
+      echo exit | sqlplus -s $USERNAME/$PASSWD@//$HOST/$SID @"$inputfile" > db_logs/db_deploy_file_ver"$version"_"$inputfilename".txt | echo "Log generation complete for script: db_deploy_file_ver"$version"_${inputfilename}"
    done     
 done
