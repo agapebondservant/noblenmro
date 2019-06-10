5888efa38b3e9cbd3cc1bb55522e31175d82b662
Tola Awofolu <oawofolu@pivotal.io>
2019-06-10 13:45:38 -0400

    Made script executable

--- scripts/validatedeploydb.sh
+++ scripts/validatedeploydb.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 ## if abort_on_db_error=true, exit if there were errors in the SQL output
 if [ `grep -r "ORA-" ./db_logs/ | wc -l` -gt 0 ] && [ $1 = 'y' ]; then
    echo "ERROR: ORA- errors encountered during execution of script(s):"
